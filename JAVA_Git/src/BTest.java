f7c993bff929e4bea921cb9ef96036246fe59805
kosmo-21 <kosmo-21@kosmo-21-PC>
2018-07-10 12:19:33 +0900

    3 commit Btest b사용자

--- JAVA_Git/src/BTest.java
+++ JAVA_Git/src/BTest.java
@@ -3,7 +3,8 @@ public class BTest {
 
 	public static void main(String[] args) {
 		System.out.println("1 commit BTest");
-		System.out.println("2 commit Btest 다른사용자");
+		System.out.println("2 commit Btest �떎瑜몄궗�슜�옄");
+		System.out.println("3 commit Btest b사용자");
 
 	}
 
